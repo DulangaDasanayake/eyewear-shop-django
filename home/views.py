3e19ce9125be87caef529218c57053e967e08fcf
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-03 20:29:27 +0530

    Views & URL's

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+def about(request):
+    return HttpResponse('<h1>this is about page')
