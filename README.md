c17464a7ad5d4191c59eebf7d5179013667be43c
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-08-13 19:54:12 +0530

    Creating superuser

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-"# lms-django"
+# Django Project
 
+### run project
 python manage.py runserver
+
+### each time you make models you have to make migrations
+python manage.py makemigrations
+python manage.py migrate
+
+### Django administration
+python manage.py createsuperuser
--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'price', 'status')
+
+# admin.site.register(Product, ProductAdmin)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'status')
+    list_filter = ('id', 'name', 'status')
+    list_editable = ('name', 'price')
+    list_per_page = 25
