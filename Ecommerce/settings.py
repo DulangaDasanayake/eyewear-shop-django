df0eef5b468324ed04dcb65992cce7e6fa44931c
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-08-13 19:25:58 +0530

    Working with models

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -123,6 +123,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+#media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    STOCK_CHOICES = (
+        ('available', 'AVAILABLE'),
+        ('out_of_stock', 'OUT_OF_STOCK')
+    )
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='images/products')
+    status = models.CharField(choices=STOCK_CHOICES, max_length=20)
+
+    def __str__(self):
+        return self.name
